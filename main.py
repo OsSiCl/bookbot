ee105933863db9afdc686f461a5bee53875f1b36
ossicl <oscroyale@gmail.com>
2024-11-07 17:33:09 +0100

    character count

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ def main():
     book_path = "github.com/OsSiCl/bookbot/books/frankenstein.txt"
     text = get_book_text(book_path)
     word_count = count_words(text)
+    character_count = count_characters(text)
     print(text)
     print(word_count)
+    print(character_count)
 
 
 def get_book_text(path):
@@ -13,5 +15,17 @@ def get_book_text(path):
 def count_words(full_text):
     words = full_text.split()
     return len(words)
+
+def count_characters(full_text):
+    dict_count={}
+
+    for item in full_text:
+        char = str(item).lower()
+
+        if char not in dict_count:
+            dict_count[char] = 1
+        else:
+            dict_count[char] += 1
+    return dict_count
     
 main()
